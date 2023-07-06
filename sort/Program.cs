58fa0ec5f85fa185a0f7b7fd057d4cf282f2b66f
Antonio <achen301@calpoly.edu>
2023-07-06 11:49:41 +0800

    Update Program.cs

--- sort/Program.cs
+++ sort/Program.cs
@@ -1,40 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
+// See https://aka.ms/new-console-template for more information
 Console.Write("insert the array to be sorted: ");
 char[] seperator = new char[] { ' ', '.', ',' };
-string[] Sarr = Console.ReadLine().Split(seperator, StringSplitOptions.RemoveEmptyEntries);
-int[] arr = new int[Sarr.Count()];
-for (int i = 0; i < Sarr.Length; i++)
+string[] unsortedStringArray = Console.ReadLine().Split(seperator, StringSplitOptions.RemoveEmptyEntries);
+int[] toBeSorted = new int[unsortedStringArray.Count()];
+for (int i = 0; i < unsortedStringArray.Length; i++)
 {
-    arr[i] = Int32.Parse(Sarr[i]);
+  toBeSorted[i] = Int32.Parse(unsortedStringArray[i]);
 }
 
-//selection sort
+// selection sort
 Console.Write("Selection sort result: ");
-for(int i = 0;i < arr.Length; i++)
+for(int i = 0;i < toBeSorted.Length; i++)
 {
-    int max_index = i;
-    for(int j = i+1; j<arr.Length; j++)
+  int max_index = i;
+  for(int j = i+1; j< toBeSorted.Length; j++)
+  {
+    if (toBeSorted[j] > toBeSorted[max_index])
     {
-        if (arr[j] > arr[max_index])
-            max_index = j;
+        max_index = j;
     }
-    int temp = arr[i];
-    arr[i] = arr[max_index];
-    arr[max_index] = temp;
-    Console.Write(arr[i] + " ");
+  }
+  int temprary = toBeSorted[i];
+  toBeSorted[i] = toBeSorted[max_index];
+  toBeSorted[max_index] = temprary;
+  Console.Write(toBeSorted[i] + " ");
 }
 
 
 // insertion sort
 Console.Write("\nInsertion sort result: ");
-for(int i = 0; i<arr.Length ; i++)
+for(int i = 0; i< toBeSorted.Length ; i++)
 {
-    int j = i + 1;
-    while(j<arr.Length&&arr[j]> arr[j-1]) 
-    {
-        int temp = arr[j-1];
-        arr[j-1] = arr[j];
-        arr[j] = temp;
-    }
-    Console.Write(arr[i]+" ");
+  int j = i + 1;
+  while(j< toBeSorted.Length&& toBeSorted[j]> toBeSorted[j-1]) 
+  {
+    int temprary = toBeSorted[j-1];
+    toBeSorted[j-1] = toBeSorted[j];
+    toBeSorted[j] = temprary;
+  }
+    Console.Write(toBeSorted[i]+" ");
 }
